4ce63ca02c45e66810ce8701a2fc2ae6a7f8b6d3
Bryan Wang <bryanhtwang@gmail.com>
2024-02-03 11:28:26 -0500

    Update Soldiers_Team_Code.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Soldiers_Team_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Soldiers_Team_Code.java
@@ -15,9 +15,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 
                         private DcMotor arm = null;
+                        private DcMotor hr = null;
+                        private DcMotor hl = null;
                         private Servo pixel = null;
                         private Servo right = null;
                         private Servo left = null;
+
                         @Override
                         public void runOpMode() throws InterruptedException {
 
@@ -25,68 +28,67 @@ import com.qualcomm.robotcore.hardware.Servo;
 
                             drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        pixel = hardwareMap.get(Servo.class, "pixel_holder");
-        right = hardwareMap.get(Servo.class, "right_claw");
-        left = hardwareMap.get(Servo.class,"left_claw");
-        pixel.setPosition(0);
-        left.setPosition(0.4);
-        right.setPosition(0);
+                            arm = hardwareMap.get(DcMotor.class, "arm");
+                            hr = hardwareMap.get(DcMotor.class, "hanger_right");
+                            hl = hardwareMap.get(DcMotor.class, "hanger_left");
+                            pixel = hardwareMap.get(Servo.class, "pixel_holder");
+                            right = hardwareMap.get(Servo.class, "right_claw");
+                            left = hardwareMap.get(Servo.class, "left_claw");
+                            pixel.setPosition(0.5);
+                            left.setPosition(0.4);
+                            right.setPosition(0);
 
                             waitForStart();
 
-        while (!isStopRequested()) {
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
-                            -gamepad1.right_stick_x
-                    )
-            );
+                            while (!isStopRequested()) {
+                                drive.setWeightedDrivePower(
+                                        new Pose2d(
+                                                -gamepad1.left_stick_y,
+                                                -gamepad1.left_stick_x,
+                                                -gamepad1.right_stick_x
+                                        )
+                                );
+
+
+                                if (gamepad2.left_bumper) {
+                                    arm.setPower(0.5);
+                                    pixel.setPosition(0);
+                                } else {
+                                    arm.setPower(0);
+                                }
+
+                                if (gamepad2.right_bumper) {
+                                    arm.setPower(-0.5);
+                                    pixel.setPosition(0.5);
+                                } else {
+                                    arm.setPower(0);
+                                }
+
+                                if (gamepad2.x) {
+                                    pixel.setPosition(0.5);
+                                }
+
+                                if(gamepad2.y) {
+                                    pixel.setPosition(0.5);
+                                    right.setPosition(0.4);
+                                    left.setPosition(0);
+                                }
+                                else{
+                                    right.setPosition(0);
+                                    left.setPosition(0.4);
+                                }
 
 
 
 
-          if (gamepad2.left_bumper) {
-              pixel.setPosition(0);
-              right.setPosition(0);
-              left.setPosition(0);
-              sleep(250);
-              arm.setPower(0.5);
-          } else {
-              arm.setPower(0);}
-
-          if (gamepad2.right_bumper) {
-              arm.setPower(-0.5);
-          } else {
-              arm.setPower(0);}
-
-
-          }
-          if (gamepad2.x) {
-              right.setPosition(-0.4);
-              left.setPosition(0.4);
-              pixel.setPosition(0);
-          }
-          else{
-              right.setPosition(0);
-              left.setPosition(0);
-              pixel.setPosition(0.5);
-          }
-
-
-
-
-
-
-
-            Pose2d poseEstimate = drive.getPoseEstimate();
-            telemetry.addData("x", poseEstimate.getX());
-            telemetry.addData("y", poseEstimate.getY());
-            telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.update();
-        }
-    }
+                                Pose2d poseEstimate = drive.getPoseEstimate();
+                                telemetry.addData("x", poseEstimate.getX());
+                                telemetry.addData("y", poseEstimate.getY());
+                                telemetry.addData("heading", poseEstimate.getHeading());
+                                telemetry.update();
+                            }
+                        }
+                    }
 
 
 
